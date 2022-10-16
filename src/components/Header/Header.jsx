bef68d363dd3248643736e738752de8790e40fe5
andrew <aklayver@gmail.com>
2022-10-16 19:57:22 -0300

    login visual finalizada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,21 @@ import "./Header.css";
 
 const Header = () => {
     return ( 
-        <header className="cabeçalho">
-            <h1>header</h1>
+        <header>
+              <img className="logo" src="../../image/logo-vittude.svg" alt="logo"></img> 
+           <nav>
+              <ul>
+                <li>Como funciona</li>
+                <li>Para você</li>
+                <li>Para psicólogos</li>
+                <li>Para empresas</li>
+                <li>Blog</li>
+              </ul>
+           <div className="visitante">
+            <a> Olá, Visitantes!</a>
+           </div>
+           </nav> 
+       
         </header>
      );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,70 +5,76 @@ import { basicSchema } from '../../schemas';
 import IconPaciente from '../IconPaciente';
 
 const onSubmit = async (values, actions) => {
-    console.log(values);
-    console.log(actions);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    actions.resetForm()
+  console.log(values);
+  console.log(actions);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  actions.resetForm()
 };
 
-function Login(){
-    const {values, errors,touched,isSubmitting, handleBlur, handleChange, handleSubmit} = useFormik({
-      initialValues:{
-        email: '',
-        password: '',
-    },   
-    validationSchema:basicSchema,  
+function Login() {
+  const { values, errors, touched, isSubmitting, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: basicSchema,
     onSubmit,
- });
-   
-console.log(errors);
-    
- return(  
-        <div className='cadastro'>
-       
-         <IconPaciente />
-        <form onSubmit={handleSubmit} autoComplete="off" className="form" >
-           
-            <div className="formField">
-              <label>Seu email</label>
-                <input 
-                id="email"
-                type="email" 
-                placeholder="Digite seu email..." 
-                onChange={handleChange}
-                value={values.email}
-                onBlur={handleBlur}
-                className={errors.email ? "input_cadastro" : "" }
-                 /> 
-              <div>
-              {errors.email && touched.email && <p className="error">{errors.email}</p>}
-            
-              </div>
-              
-                 <label>Sua senha</label>
-                 <input
-                id="password"
-                type="password" 
-                placeholder="Digite sua senha..."
-                onChange={handleChange}
-                value={values.password} 
-                onBlur={handleBlur} 
-                className={errors.password ? "input_cadastro" : "" } />
-            
-              <div>
-              {errors.password && touched.password ? <p>{errors.password}</p> : null}
-              </div>                      
-            </div>             
+  });
 
-            <div className="enviar-cadastro">
-            <div className="link-login">
-                 <a href="#">Esqueci minha senha</a>
-                </div>
-                <button disabled={isSubmitting} className="button" type="submit">Entrar</button>            
+  console.log(errors);
+
+  return (
+
+    <div className='cadastro'>
+      <h1 className='text-login'>Faça seu login</h1>
+      <div className='cinza'>
+        <IconPaciente />
+        <form onSubmit={handleSubmit} autoComplete="off" className="form" >
+
+          <div className="formField">
+            <label>Email*</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Digite seu email..."
+              onChange={handleChange}
+              value={values.email}
+              onBlur={handleBlur}
+
+            />
+            <div>
+              {errors.email && touched.email && <p className="error">{errors.email}</p>}
+
             </div>
+          </div>
+
+          <div className="formField">
+
+            <label>Senha*</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Digite sua senha..."
+              onChange={handleChange}
+              value={values.password}
+              onBlur={handleBlur}
+            />
+
+            <div>
+              {errors.password && touched.password && <p className="error">{errors.password}</p>}
+            </div>
+          </div>
+
+          <div className="enviar-cadastro">
+            <div className="link-login">
+              <a href="#">Esqueci minha senha</a>
+            </div>
+            <button disabled={isSubmitting} className="button" type="submit">Entrar</button>
+          </div>
         </form>
-    </div>     
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Login;
